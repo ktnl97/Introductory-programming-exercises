f047cbc0349d22ef115f1a3ed1ec91412086fdfe
ktnl97 <39978547+ktnl97@users.noreply.github.com>
2018-06-07 20:42:37 +0530

    Name is printed in the middle.

--- Diamonds.java
+++ Diamonds.java
@@ -12,6 +12,10 @@ public class Diamonds
         n=sc.nextInt();
         System.out.println();
         diamond(n);
+        System.out.print("For Diamond with Name n=");
+        n=sc.nextInt();
+        System.out.println();
+        diamondWithName(n);
     }
     public static void isoscelesTriangle(int n)
     {
@@ -33,6 +37,21 @@ public class Diamonds
             for(k=0;k<Math.abs(n-1-i);k++)
                 System.out.print(" ");
             for(j=0;j<2*(n-k)-1;j++)// 2(n-1) - 2(k+1) -> total entries -total spaces
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+    public static void diamondWithName(int n)
+    {
+    int i,j,k;
+     for(i=0;i<2*n-1;i++)
+        {
+            for(k=0;k<Math.abs(n-1-i);k++)
+                System.out.print(" ");
+            if(k==0)
+                System.out.print("NITHYA");
+            else
+            for(j=0;j<2*(n-k)-1;j++)
                 System.out.print("*");
             System.out.println();
         }
