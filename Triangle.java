40279ab8efb1c487c97d29ca0583ed4f224c98c6
ktnl97 <39978547+ktnl97@users.noreply.github.com>
2018-06-07 20:13:25 +0530

    Single asterisk mark is printed.

---
+++ Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle
+{
+    public static void main(String[] args) 
+    {
+        singleAsterisk();
+    }
+    public static void singleAsterisk()
+    {
+    System.out.println("*");
+    }
+}
