308635bc6b7b534d6b4124517fbeaeb81d8a6dd8
ktnl97 <39978547+ktnl97@users.noreply.github.com>
2018-06-07 20:15:31 +0530

    Horizontal line of asterisks is printed.

--- Triangle.java
+++ Triangle.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
 public class Triangle
 {
     public static void main(String[] args) 
     {
         singleAsterisk();
+        Scanner sc=new Scanner(System.in);
+        System.out.print("n=");
+        int i=sc.nextInt();
+        System.out.println();   
+        horizontalAsterisk(i);
     }
     public static void singleAsterisk()
     {
     System.out.println("*");
+    }
+    public static void horizontalAsterisk(int n)
+    {
+    String result=new String(new char[n]).replace('\0','*');
+    System.out.print(result);
     }
 }
