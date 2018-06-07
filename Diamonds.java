004335a6aeb4a4425e0ee779b81c92b58b4a4a75
ktnl97 <39978547+ktnl97@users.noreply.github.com>
2018-06-07 20:38:11 +0530

    Diamond added.

--- Diamonds.java
+++ Diamonds.java
@@ -4,10 +4,14 @@ public class Diamonds
     public static void main(String[] args) 
     {
         Scanner sc=new Scanner(System.in);
-        System.out.print("n=");
+        System.out.print("For Isosceles n=");
         int n=sc.nextInt();
         System.out.println();
         isoscelesTriangle(n);
+        System.out.print("For Diamond n=");
+        n=sc.nextInt();
+        System.out.println();
+        diamond(n);
     }
     public static void isoscelesTriangle(int n)
     {
@@ -20,5 +24,17 @@ public class Diamonds
                 System.out.print("* ");
             System.out.println();
         }        
+    }
+    public static void diamond(int n)
+    {
+    int i,j,k;
+        for(i=0;i<2*n-1;i++)
+        {
+            for(k=0;k<Math.abs(n-1-i);k++)
+                System.out.print(" ");
+            for(j=0;j<2*(n-k)-1;j++)// 2(n-1) - 2(k+1) -> total entries -total spaces
+                System.out.print("*");
+            System.out.println();
+        }
     }
 }
